ca8252b2cd7cf774cd29bb4a9dbc412f13ee2317
Hussain Ravat <hussain.ravat786@gmail.com>
2025-05-19 05:39:44 +1200

    Completed HW 3 till Q3 ...Setup of Mage and MLFlow  done

---
+++ init-db/01_create_mage_schema.sql
@@ -0,0 +1,5 @@
+\echo 'Creating schema mage...'
+
+CREATE SCHEMA IF NOT EXISTS mage AUTHORIZATION postgres;
+
+\echo 'Schema mage created or already exists.'
---
+++ init-db/02_create_mlflow_schema.sql
@@ -0,0 +1,6 @@
+\echo 'Creating mlflow mage...'
+
+
+CREATE SCHEMA IF NOT EXISTS mlflow AUTHORIZATION postgres;
+
+\echo 'Schema mlflow created or already exists.'
